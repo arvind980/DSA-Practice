e4d2e4cfc13d5d8b5c898a5c6375aebcacdbba86
arvind980 <arvind.rajput0524@gmail.com>
2026-01-25 22:32:09 +0530

    FindFirstAndLast Position

---
+++ BinarySearch/FindFirstAndLastPosition.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+func findFirstAndLastIndex(nums:[Int],target:Int)->[Int]{
+    func findFirts()->Int{
+        var left = 0
+        var right = nums.count - 1
+        var ans = -1
+        
+        while left <= right{
+            var mid = left + (right - left)/2
+            
+            if nums[mid] == target{
+                ans = mid
+                right = mid - 1
+            }else if target > nums[mid]{
+                left = left + 1
+            }else{
+                right = mid - 1
+            }
+        }
+        
+        return ans
+    }
+    
+    func findLast()->Int{
+        var left = 0
+        var right = nums.count - 1
+        var ans = -1
+        
+        while left <= right{
+            var mid = left + (right - left)/2
+            
+            if nums[mid] == target{
+                ans = mid
+                left = mid + 1
+            }else if target > nums[mid]{
+                left = left + 1
+            }else{
+                right = mid - 1
+            }
+        }
+        
+        return ans
+    }
+    
+    return [findFirts(), findLast()]
+}
+
+print(findFirstAndLastIndex(nums:[2,3,5,5,8,9],target:5))
